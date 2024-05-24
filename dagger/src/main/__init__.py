3c33b5a0aea713a6fb5cb99cc378186955e1284d
Prashant Bhapkar <60314439+Prashant-Bhapkar@users.noreply.github.com>
2024-05-24 05:03:09 -0700

    Add files via upload

---
+++ dagger/src/main/__init__.py
@@ -0,0 +1,23 @@
+"""A generated module for Demo1 functions
+
+This module has been generated via dagger init and serves as a reference to
+basic module structure as you get started with Dagger.
+
+Two functions have been pre-created. You can modify, delete, or add to them,
+as needed. They demonstrate usage of arguments and return types using simple
+echo and grep commands. The functions can be called from the dagger CLI or
+from one of the SDKs.
+
+The first line in this comment block is a short description line and the
+rest is a long description with more detail on the module's purpose or usage,
+if appropriate. All modules should have a short description.
+"""
+
+import dagger
+from dagger import dag, function, object_type
+
+@object_type
+class Demo1:
+    @function
+    def hello_world(self) -> str:
+        return "Hello Daggernauts!"
